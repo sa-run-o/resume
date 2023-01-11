b11cf5af387e3a730902b25fb2e820f25b58b0c0
GoPomelo Co <gopomelo@Run-MacBook-Air.local>
2023-01-11 09:34:06 +0700

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { motion } from "framer-motion";
+import styled from "styled-components";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SContainer>
+      <SMotionBox
+        animate={{
+          scale: [1, 2, 2, 3, 3],
+          rotate: [180, 0, 180, 180, 0],
+          borderRadius: ["0%", "0%", "50%", "0%", "50%"],
+        }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          times: [0, 0.2, 0.5, 0.8, 1],
+          repeat: Infinity,
+          repeatDelay: 1,
+        }}
+      />
+    </SContainer>
   );
 }
 
 export default App;
+const SMotionBox = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background: green;
+`;
+const SContainer = styled.div`
+  background-color: blue;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;

b810f11131faf4dd63003c13b8b7429c36e3c165
GoPomelo Co <gopomelo@Run-MacBook-Air.local>
2023-01-16 14:54:18 +0700

    update

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,11 +1,13 @@
 export const theme = {
   sun: {
-    main: "rgba(198,209,221,1.00)",
+    default_main: "rgba(198,209,221,1.00)",
+    main: "rgba(183,60,88,1.00)",
     text: "rgba(183,60,88,1.00)",
     sub: "rgba(24,25,26,1.00)",
   },
   moon: {
-    main: "rgba(24,25,26,1.00)",
+    default_main: "rgba(24,25,26,1.00)",
+    main: "rgba(58,59,60,1.00)",
     text: "rgba(58,59,60,1.00)",
     sub: "rgba(228,230,235,1.00)",
   },

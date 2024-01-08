e8a7e335a64ccbbd3fc1cf688d76ca5fa1f44084
Sarun Olankranok <gopomelo@MacBook-Air.local>
2024-01-08 14:05:48 +0700

    start

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+function App() {
+  return <SContainer></SContainer>;
+}
+
+export default App;
+const SContainer = styled.div``;
